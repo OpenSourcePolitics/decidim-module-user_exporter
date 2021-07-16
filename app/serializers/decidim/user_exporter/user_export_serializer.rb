8554e1bd5e1b896e9e0d20f52a1b238cd029c4d6
quentinchampenois <26109239+Quentinchampenois@users.noreply.github.com>
2021-07-16 11:37:38 +0200

    add specs for serializer

--- app/serializers/decidim/user_exporter/user_export_serializer.rb
+++ app/serializers/decidim/user_exporter/user_export_serializer.rb
@@ -7,7 +7,37 @@ module Decidim
       # Public: Exports a hash with the serialized data for the user including
       # extra user fields
       def serialize
-        super.merge(extra_fields)
+        {
+          id: resource.id,
+          email: resource.email,
+          name: resource.name,
+          nickname: resource.nickname,
+          organization: {
+            id: resource.organization.try(:id),
+            name: resource.organization.try(:name)
+          },
+          invitation_created_at: resource.invitation_created_at,
+          invitation_sent_at: resource.invitation_sent_at,
+          invitation_accepted_at: resource.invitation_accepted_at,
+          invited_by: {
+            id: resource.invited_by_id,
+            type: resource.invited_by_type
+          },
+          invitations_count: resource.invitations_count,
+          sign_in_count: resource.sign_in_count,
+          last_sign_in_at: resource.last_sign_in_at,
+          created_at: resource.created_at,
+          updated_at: resource.updated_at,
+          confirmed_at: resource.confirmed_at,
+          unconfirmed_email: resource.unconfirmed_email,
+          delete_reason: resource.delete_reason,
+          deleted_at: resource.deleted_at,
+          admin: resource.admin,
+          accepted_tos_version: resource.accepted_tos_version,
+          managed: resource.managed,
+          officialized_at: resource.officialized_at,
+          officialized_as: resource.officialized_as
+        }.merge(extra_fields)
       end
 
       def extra_fields
